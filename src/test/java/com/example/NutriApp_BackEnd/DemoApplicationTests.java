fd219aa3ff68cef53ff522366771ffc79d3095cd
William Adjandra <willihogan@gmail.com>
2018-08-16 23:17:34 +0700

    initial commit

---
+++ src/test/java/com/example/NutriApp_BackEnd/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.NutriApp_BackEnd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
